633a37ec400b9102be0cc2ef2cc6a9f8ff76ad02
Kurai021 <samahel021@gmail.com>
2014-07-05 10:44:50 -0500

    font awesome social buttons added and some fixes in the rss feed

--- build/scripts/receiptverifier.init.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    installPrompt = mozmarket.receipts.Prompter({
-        storeURL: "https://marketplace.firefox.com/app/btcviewer/",
-        supportHTML: '<a href="mailto:alexanderarmuelles@riseup.net">email alexanderarmuelles@riseup.net</a>',
-        verify: true,
-        verifierOptions: {onlog: mozmarket.receipts.Verifier.consoleLogger}
-    });
-})();
